8512837a9ae07f7952cea5db70606523164fd266
Nasim Reza <nasim8382@gmail.com>
2022-05-23 15:10:37 +0600

    server running set up done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Toolex server is running successfully');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+});
