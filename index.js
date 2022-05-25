a02ca9396f3c22052989f192aa3b0cab8af8f331
Nasim Reza <nasim8382@gmail.com>
2022-05-25 12:17:16 +0600

    admin created

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,11 +15,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
 const run = async() => {
     try{
         await client.connect();
         const toolsCollection = client.db('toolex').collection('tools');
         const reviewsCollection = client.db('toolex').collection('reviews');
+        const userCollection = client.db('toolex').collection('users');
 
         // tools collection
         app.get('/tool', async(req, res) => {
@@ -28,6 +45,14 @@ const run = async() => {
             res.send(tools);
         })
 
+        // individual tool
+        app.get('/tool/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tool = await toolsCollection.findOne(query);
+            res.send(tool);
+        });
+
         // reviews collection
         app.get('/review', async(req, res) => {
             const query = {};
@@ -35,6 +60,48 @@ const run = async() => {
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+          });
+
+          app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+          })
+
+          app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+              const filter = { email: email };
+              const updateDoc = {
+                $set: { role: 'admin' },
+              };
+              const result = await userCollection.updateOne(filter, updateDoc);
+              res.send(result);
+            }
+            else{
+              res.status(403).send({message: 'forbidden'});
+            }
+          })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+          const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+          res.send({ result, token });
+          })
     }
     finally{
 
