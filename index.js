35416bd00fa09959a51ebd83622bed9a624fe4bc
Nasim Reza <nasim8382@gmail.com>
2022-05-25 22:17:21 +0600

    product delete code done

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ const run = async() => {
         const reviewsCollection = client.db('toolex').collection('reviews');
         const userCollection = client.db('toolex').collection('users');
 
+        const verifyAdmin = async (req, res, next) => {
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({
+            email: requester,
+          });
+          if (requesterAccount.role === "admin") {
+            next();
+          } else {
+            res.status(403).send({ message: "forbidden" });
+          }
+        };
+
         // tools collection
         app.get('/tool', async(req, res) => {
             const query = {};
@@ -51,6 +63,14 @@ const run = async() => {
             const query = {_id: ObjectId(id)};
             const tool = await toolsCollection.findOne(query);
             res.send(tool);
+        });
+
+        // delete a product
+        app.delete("/tool/:id", verifyJWT, async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: ObjectId(id) };
+          const result = await toolsCollection.deleteOne(query);
+          res.send(result);
         });
 
         // reviews collection
@@ -80,7 +100,7 @@ const run = async() => {
             res.send({admin: isAdmin})
           })
 
-          app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+          app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const requester = req.decoded.email;
             const requesterAccount = await userCollection.findOne({ email: requester });
